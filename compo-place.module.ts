9e8cbd5eb43481bdcfbc602844379faa226fe20f
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-22 16:59:53 -0500

    creando index.ts

---
+++ compo-place.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }     from '@angular/core';
+import { CompoPlaceComponent }  from './compo-place.component';
+
+@NgModule({
+  declarations: [
+    CompoPlaceComponent
+      ],
+    exports: [CompoPlaceComponent]
+})
+export class CompoPlaceModule{}
